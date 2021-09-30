ee8aa2cd50c9ec953142581fb0d7c9228f0bbcda
David Naranjo <davidn182@hotmail.com>
2021-09-30 10:32:41 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'OCloC'
+copyright = '2021, Naranjo'
+author = 'David Naranjo'
 
 release = '0.1'
 version = '0.1.0'
